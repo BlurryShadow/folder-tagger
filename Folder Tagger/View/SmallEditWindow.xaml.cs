db0d5772718e6bea8e6ff1382db8e0addd33f664
Xuan Hieu <xuanhieu1479@gmail.com>
2019-12-18 19:30:37 +0700

    Done edit artist and group.

--- Folder Tagger/View/SmallEditWindow.xaml.cs
+++ Folder Tagger/View/SmallEditWindow.xaml.cs
@@ -21,12 +21,54 @@ namespace Folder_Tagger
     {
         private string type = "";
         private string folder = "";
+        private string artist = "";
+        private string group = "";
         public SmallEditWindow(string type, string folder)
         {
             InitializeComponent();
             this.type = type;
             this.folder = folder;
-            tbInput.Text = type + " - " + folder;
+            getFolderData();
+        }
+
+        private void getFolderData()
+        {
+            using (var db = new Model1())
+            {
+                var query = db.Folders
+                              .Where(f => f.Location == folder)
+                              .Select(f => new { f.Artist, f.Group })
+                              .FirstOrDefault();
+
+                artist = query.Artist;
+                group = query.Group;
+
+                if (type.Equals("Artist")) tbInput.Text = artist;
+                if (type.Equals("Group")) tbInput.Text = group;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            string input = tbInput.Text;
+            if (string.IsNullOrWhiteSpace(input)) input = null;
+
+            using (var db = new Model1())
+            {
+                var query = db.Folders
+                              .Where(f => f.Location == folder)
+                              .Select(f => f)
+                              .ToList();
+
+                query.ForEach(f => 
+                { 
+                    if (type.Equals("Artist")) f.Artist = input;
+                    if (type.Equals("Group")) f.Group = input;
+                });
+
+                db.SaveChanges();
+                Close();
+            }
         }
     }
 }
