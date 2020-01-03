b69bd5dcfe1d53f05d6cfe82f55f8f9b6a9909f7
Xuan Hieu <xuanhieu1479@gmail.com>
2020-01-04 05:19:23 +0700

    Refactor add tag window.

--- Folder Tagger/View/AddTagWindow.xaml.cs
+++ Folder Tagger/View/AddTagWindow.xaml.cs
@@ -9,15 +9,17 @@ namespace Folder_Tagger
 {
     public partial class AddTagWindow : Window
     {
-        private readonly List<string> folderList = new List<string>();
-        private readonly List<Tag> allTag = new List<Tag>();
+        private readonly List<string> locations;
+        private readonly List<Tag> allTags;
+        private readonly int numberCharactersToActiveSuggestion = 3;
+
         private readonly FolderController fc = new FolderController();
         private readonly TagController tc = new TagController();
-        public AddTagWindow(List<string> folderList)
+        public AddTagWindow(List<string> locations)
         {
             InitializeComponent();
-            this.folderList = folderList;
-            allTag = tc.GetTags("all", "mostUsed");
+            this.locations = locations;
+            allTags = tc.GetTags("all", "mostUsed");
 
             Loaded += (sender, e) => tbInput.Focus();
             PreviewKeyDown += (sender, e) => { if (e.Key == Key.Escape) Close(); };
@@ -34,15 +36,19 @@ namespace Folder_Tagger
         {
             if (!string.IsNullOrWhiteSpace(tbInput.Text))
             {
+                //The last part of TextBox Text, behind ', '
+                //Not the whole TextBox string
+                //Nor where the cursor is located at
+                //Example: "a1, a2, a3|, a4, a5"    | = cursor   currentTag = a5
                 string currentTag = tbInput.Text.Split(new string[] { ", " }, StringSplitOptions.None).Last().ToLower().Trim();
-                if (string.IsNullOrEmpty(currentTag) || currentTag.Length < 3)
+                if (string.IsNullOrEmpty(currentTag) || currentTag.Length < numberCharactersToActiveSuggestion)
                 {
                     popupAutoComplete.IsOpen = false;
                     return;
                 }
 
-                var suggestedTag = allTag.Where(t => t.TagName.Contains(currentTag)).ToList();
-                int suggestedTagCount = suggestedTag.Count;
+                var suggestedTags = allTags.Where(t => t.TagName.Contains(currentTag)).ToList();
+                int suggestedTagCount = suggestedTags.Count;
                 if (suggestedTagCount == 0)
                 {
                     popupAutoComplete.IsOpen = false;
@@ -50,18 +56,20 @@ namespace Folder_Tagger
                 }
 
                 popupAutoComplete.Height = suggestedTagCount > 10 ? 250 : suggestedTagCount * 25 + 10;
-                listboxAutoComplete.ItemsSource = suggestedTag;
+                listboxAutoComplete.ItemsSource = suggestedTags;
                 popupAutoComplete.IsOpen = true;
             }
         }
 
-        private void TextBoxInput_KeyDown(object sender, KeyEventArgs e)
+        private void TextBoxInput_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+            //Jump to suggestion box when press Down Key
             if (e.Key == Key.Down && popupAutoComplete.IsOpen)
             {
                 listboxAutoComplete.SelectedIndex = 0;
                 var item = (ListBoxItem)listboxAutoComplete.ItemContainerGenerator.ContainerFromItem(listboxAutoComplete.SelectedItem);
                 item.Focus();
+                //Prevent focus from jumping to 2nd element
                 e.Handled = true;
             }
 
@@ -73,21 +81,26 @@ namespace Folder_Tagger
             }
         }
 
-        private void ListBox_KeyDown(object sender, KeyEventArgs e)
+        private void ListBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+            //Jump back to TextBox when press Up Key at first element
             if (e.Key == Key.Up && listboxAutoComplete.SelectedIndex == 0)
                 tbInput.Focus();
 
-            if (e.Key == Key.S) //Disable 'S' key default event
+            //Disable 'S' key default event
+            //Which is the same as Down key
+            //But return to first element when pressed at last one
+            if (e.Key == Key.S)
                 e.Handled = true;
 
             if (e.Key == Key.Enter)
             {
                 Tag t = listboxAutoComplete.SelectedItem as Tag;
-                int lastPosition = tbInput.Text.LastIndexOf(", ");
+                int indexPosition = tbInput.Text.LastIndexOf(", ");
                 try
                 {
-                    tbInput.Text = lastPosition == -1 ? t.TagName : tbInput.Text.Substring(0, lastPosition + 2) + t.TagName;
+                    //indexPosition == -1 means TextBox does not have any tag yet
+                    tbInput.Text = indexPosition == -1 ? t.TagName : tbInput.Text.Substring(0, indexPosition + 2) + t.TagName;
                     tbInput.Text += ", ";
                     tbInput.CaretIndex = tbInput.Text.Length;
                 } catch(Exception exception)
@@ -95,6 +108,8 @@ namespace Folder_Tagger
                     System.Windows.Forms.MessageBox.Show(exception.Message);
                 }
                 tbInput.Focus();
+                //Add Button is default
+                //Cancel Enter Key Down event to prevent premature trigger
                 e.Handled = true;
             }
         }
@@ -107,15 +122,14 @@ namespace Folder_Tagger
                 return;
             }
 
-            List<string> tagList = tbInput.Text.Split(new string[] { ", " }, StringSplitOptions.None).ToList();
-
+            List<string> tags = tbInput.Text.Split(new string[] { ", " }, StringSplitOptions.None).ToList();
             using (var db = new Model1())
-                foreach (string location in folderList)
-                    foreach (string tag in tagList)
+                foreach (string location in locations)
+                    foreach (string tag in tags)
                     {
                         string currentTag = tag.Trim().ToLower();
-                        if (string.IsNullOrEmpty(currentTag)) continue;
-
+                        if (string.IsNullOrEmpty(currentTag))
+                            continue;
                         if (db.Folders.Any(f => f.Location == location && f.Tags.Any(t => t.TagName == currentTag)))
                             continue;
 
