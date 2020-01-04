5f7010e79300fac79490a483858d9bd42bde2108
Xuan Hieu <xuanhieu1479@gmail.com>
2020-01-04 23:40:36 +0700

    Refactor info window according to edit artist / group window.

--- Folder Tagger/View/InfoWindow.xaml.cs
+++ Folder Tagger/View/InfoWindow.xaml.cs
@@ -8,9 +8,8 @@ namespace Folder_Tagger
 {
     public partial class InfoWindow : Window
     {
-        private readonly string infoType = "";
-        private string oldInput = "";
-        private List<string> infoList;
+        private readonly string infoType;
+        private List<string> infos;
 
         private readonly TagController tc = new TagController();
         private readonly FolderController fc = new FolderController();
@@ -22,43 +21,53 @@ namespace Folder_Tagger
             switch (infoType)
             {
                 case "Tag":
-                    infoList = tc.GetTags().Select(t => t.TagName).ToList();                    
+                    infos = tc.GetTags().Select(t => t.TagName).ToList();                    
                     break;
                 case "Artist":
-                    infoList = fc.GetArtists();
+                    infos = fc.GetArtists();
                     break;
                 case "Group":
-                    infoList = fc.GetGroups();
+                    infos = fc.GetGroups();
                     break;
             }            
-            listboxInfo.ItemsSource = infoList;
+            listboxInfo.ItemsSource = infos;
 
-            Loaded += (sender, e) => tbSuggest.Focus();
+            Loaded += (sender, e) => tbSuggestion.Focus();
+            PreviewKeyDown += (sender, e) => { if (e.Key == Key.Escape) Close(); };
         }
 
-        private void TextBoxSort_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBoxSuggestion_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string currentSort = tbSuggest.Text.ToLower().Trim();
-            if (string.IsNullOrEmpty(currentSort))
-                listboxInfo.ItemsSource = infoList;
+            string currentSuggestion = tbSuggestion.Text.ToLower().Trim();
+            if (string.IsNullOrEmpty(currentSuggestion))
+                listboxInfo.ItemsSource = infos;
             else
-                listboxInfo.ItemsSource = infoList.Where(i => i.Contains(currentSort));
+                listboxInfo.ItemsSource = infos.Where(i => i.Contains(currentSuggestion));
         }
-        private void TextBoxInput_GotKeyboardFocus(object sender, RoutedEventArgs e)
+
+        private void TextBoxInputInsideListBox_PreviewMouseUp(object sender, MouseButtonEventArgs e) //For editing with mouse
         {
             TextBox tb = (TextBox)sender;
-            oldInput = tb.Text;
+            tb.SelectAll();
         }
 
-        private void TextBoxInput_KeyDown(object sender, KeyEventArgs e)
+        private void TextBoxInputInsideListBox_GotFocus(object sender, RoutedEventArgs e) //For editing with keyboard
+        {
+            TextBox tb = (TextBox)sender;
+            tb.SelectAll();
+        }
+
+        private void TextBoxInputInsideListBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
-                Keyboard.ClearFocus();
+                MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
         }
 
-        private void TextBoxInput_LostKeyboardFocus(object sender, RoutedEventArgs e)
+        //Refer to FullEditWindow for more information
+        private void TextBoxInputInsideListBox_LostFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
+            string oldInput = tb.Tag.ToString();
             string newInput = string.IsNullOrWhiteSpace(tb.Text) ? null : tb.Text.Trim().ToLower();
 
             if (newInput == oldInput.Trim().ToLower())
@@ -69,50 +78,63 @@ namespace Folder_Tagger
 
             using (var db = new Model1())
             {
-                List<Folder> folderList;
+                List<Folder> folders;
                 switch (infoType)
-                {                    
+                {
                     case "Tag":
                         Tag oldTag = tc.GetTagByName(oldInput, db);
+                        //Delete oldTag if users input empty string
                         if (newInput == null)
                             db.Tags.Remove(oldTag);
                         else
                         {
                             Tag newTag = tc.GetTagByName(newInput, db);
+                            //If there is not any tag that has the same name as newInput
+                            //Simply update oldTag TagName into newInput
+                            //We need to check this because the primary key is TagID, not TagName
+                            //Therefore multiple tags with the same name is a possibility
                             if (newTag == null)
                                 oldTag.TagName = newInput;
+                            //If there is already a tag with TagName == newInput
+                            //Transfer all folders belong to oldTag to newTag and then delete oldTag
+                            //Again because the primary key is TagID, not TagName
                             if (newTag != null)
                             {
-                                folderList = db.Folders.Where(f => f.Tags.Any(t => t.TagName == oldInput)).ToList();
+                                folders = db.Folders.Where(f => f.Tags.Any(t => t.TagName == oldInput)).ToList();
                                 db.Tags.Remove(oldTag);
-                                folderList.ForEach(f => f.Tags.Add(newTag));
+                                folders.ForEach(f => f.Tags.Add(newTag));
                             }
                         }
                         break;
+                    //Unlike Tag which has many-to-many relationship with Folder
+                    //Artist and Group are property of Folder
+                    //Therefore no need to check duplicate and deleting just means update them to null
+                    //But also when updating we have to update each folder individually
                     case "Artist":
-                        folderList = fc.GetFoldersByArtist(oldInput, db);
-                        folderList.ForEach(f => fc.UpdateArtist(f.Location, newInput));
+                        folders = fc.GetFoldersByArtist(oldInput, db);
+                        folders.ForEach(f => fc.UpdateArtist(f.Location, newInput));
                         break;
                     case "Group":
-                        folderList = fc.GetFoldersByGroup(oldInput, db);
-                        folderList.ForEach(f => fc.UpdateGroup(f.Location, newInput));
+                        folders = fc.GetFoldersByGroup(oldInput, db);
+                        folders.ForEach(f => fc.UpdateGroup(f.Location, newInput));
                         break;
                 }
-
                 db.SaveChanges();
+
                 switch (infoType)
                 {
                     case "Tag":
-                        infoList = tc.GetTags().Select(t => t.TagName).ToList();
+                        infos = tc.GetTags().Select(t => t.TagName).ToList();
                         break;
                     case "Artist":
-                        infoList = fc.GetArtists();
+                        infos = fc.GetArtists();
                         break;
                     case "Group":
-                        infoList = fc.GetGroups();
+                        infos = fc.GetGroups();
                         break;
                 }
-                listboxInfo.ItemsSource = infoList;
+                //ItemsSource will be re-generated anyway so updating TextBox Tag for new oldInput is unnecessary
+                listboxInfo.ItemsSource = infos;
             }
         }
     }
